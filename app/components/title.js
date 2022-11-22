2e64eaabdcd070d8ae0e45af3473bd430c1872aa
HelioGonzales <heliogonzalesr@gmail.com>
2022-11-22 01:11:16 +0100

    fix: main link

--- app/components/title.js
+++ app/components/title.js
@@ -4,8 +4,7 @@ export function Title() {
   const $titleLink = document.createElement("a");
   const $h1 = document.createElement("h1");
   $titleLink.appendChild($h1);
-  $titleLink.href = "/book-shop/#/";
-  // $titleLink.href = "/#" || "book-shop/#/";
+  $titleLink.href = "#";
   $titleLink.classList.add("title-link");
   $h1.textContent = `${book_api.NAME.toUpperCase()}`;
 
